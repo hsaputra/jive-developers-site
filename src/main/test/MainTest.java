bbc6ef9631afad6617fe133b2da4d6a9b85d9cdd
Henry Saputra <henry.saputra@jivesoftware.com>
2012-09-27 18:34:25 -0700

    Add Jetty plugin

--- src/main/test/MainTest.java
+++ src/main/test/MainTest.java
@@ -8,10 +8,10 @@ public class MainTest {
   public static void main(String[] args) {
     System.out.println("Hello World");
 
-    JiveCommunityRssFetcher fetcher = new JiveCommunityRssFetcher();
+    JiveCommunityRssFetcher fetcher = new JiveCommunityRssFetcher("https://community.jivesoftware.com/blogs");
 
     try {
-      fetcher.getFeedsFromJiveContentType("blogs", "posts");
+      fetcher.getFeedsFromJiveContentType("jivetalks", "posts");
     } catch (Exception e) {
       e.printStackTrace();
     }
